73389b8cffb3ed25e015bf66e325d1b673f3a564
egeklcsln <ege.klcsln1254@gmail.com>
2023-12-08 14:49:35 +0300

    fsa

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,13 @@
 <?php
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','mvc');
+
+
+
+
 define('APPROOT', dirname(dirname(__FILE__))) ;
 define('URLROOT','http://localhost/PHP-MVC');
 define('SITENAME','PHP-MVC');
